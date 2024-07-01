45e298aeac5814f805c33445aa45c176d836992a
Kintttt <topekint@gmail.com>
2024-07-01 18:20:24 +0100

    chore : created project structure

---
+++ src/main/java/com/example/googlecalendareventservice/GoogleCalendarEventServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.googlecalendareventservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleCalendarEventServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoogleCalendarEventServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/googlecalendareventservice/GoogleCalendarEventServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.googlecalendareventservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleCalendarEventServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
